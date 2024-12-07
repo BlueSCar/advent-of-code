a52dc2609b9b2de57ef3029dae3e1b2d1e4ae8ee
Bill Radjewski <radjewwj@gmail.com>
2024-12-07 10:20:00 -0500

    2024 day 7

---
+++ 2024/day07/part2.py
@@ -0,0 +1,18 @@
+file = open('2024/day07/input.txt')
+lines = [dict(
+    solution=int(l.split(":")[0].strip()),
+    terms = [int(x.strip()) for x in l.split(":")[1].strip().split(" ")]
+    ) for l in file.readlines()]
+
+def is_valid(solution, current_solution, terms):
+    if len(terms) == 1:
+        return current_solution + terms[0] == solution or current_solution * terms[0] == solution or int(str(current_solution) + str(terms[0])) == solution
+    else:
+        return is_valid(solution, current_solution + terms[0], terms[1:]) or is_valid(solution, current_solution * terms[0], terms[1:]) or is_valid(solution, int(str(current_solution) + str(terms[0])), terms[1:])
+
+sum = 0
+for line in lines:
+    if (is_valid(line['solution'], line['terms'][0], line['terms'][1:])):
+        sum += line['solution']
+
+print(sum)
