ef3fa31078fab02dfd8cf574e2b2f49e06886102
Bill Radjewski <radjewwj@gmail.com>
2024-12-12 09:54:23 -0500

    2024 day 8

---
+++ 2024/day08/part2.py
@@ -0,0 +1,38 @@
+file = open('2024/day08/input.txt')
+grid = [list(l.strip()) for l in file.readlines()]
+
+frequencies = dict()
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] != '.':
+            if grid[i][j] not in frequencies:
+                frequencies[grid[i][j]] = [(i, j)]
+            else:
+                frequencies[grid[i][j]].append((i, j))
+                
+antinodes = set()
+grid_height = len(grid)
+grid_width = len(grid[0])
+
+for key in frequencies:
+    antennas = frequencies[key]
+    for i in range(len(antennas) - 1):
+        for j in range(i + 1, len(antennas)):
+            height_diff = antennas[i][0] - antennas[j][0]
+            width_diff = antennas[i][1] - antennas[j][1]
+            
+            antinodes.add(antennas[i])
+            antinodes.add(antennas[j])
+            
+            point1 = (antennas[i][0] + height_diff, antennas[i][1] + width_diff)
+            while 0 <= point1[0] <= grid_height - 1 and 0 <= point1[1] <= grid_width - 1:
+                antinodes.add(point1)
+                point1 = (point1[0] + height_diff, point1[1] + width_diff)
+                
+            point2 = (antennas[j][0] - height_diff, antennas[j][1] - width_diff)
+            while 0 <= point2[0] <= grid_height - 1 and 0 <= point2[1] <= grid_width - 1:
+                antinodes.add(point2)
+                point2 = (point2[0] - height_diff, point2[1] - width_diff)
+
+print(len(antinodes))
